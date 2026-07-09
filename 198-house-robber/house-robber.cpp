d2ef5a6d716d9ea93897ffee57938d785423c194
HiteshKPatel674 <147538687+HiteshKPatel674@users.noreply.github.com>
2026-07-09 21:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.18%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -18,6 +18,7 @@ public:
         // ans=solve(nums,0);
         // return ans;
         int n=nums.size();
+        if(n == 1) return nums[0];
         vector<int>dp(n,-1);
         return solve(nums,0,dp);       
     }
