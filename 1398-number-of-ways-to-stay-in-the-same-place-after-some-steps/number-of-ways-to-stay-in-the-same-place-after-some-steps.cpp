216ba4ceba692a905873dd29b35e9d4bc653d9ad
HiteshKPatel674 <147538687+HiteshKPatel674@users.noreply.github.com>
2026-07-12 19:53:07 +0530

    Time: 11 ms (64.41%) | Memory: 21.7 MB (15.10%) - LeetSync

---
+++ 1398-number-of-ways-to-stay-in-the-same-place-after-some-steps/number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -0,0 +1,41 @@
+class Solution {
+const long long MOD = 1e9 + 7;
+long long solve(int stepsLeft,int pos,int arrLen,vector<vector<long long>>&dp)
+{
+    if(stepsLeft==0)
+    {
+        if(pos==0) return 1;
+        else return 0;
+    }
+
+    if (pos > stepsLeft) return 0;
+    //as u can't reach the index 0
+
+    if(dp[stepsLeft][pos]!=-1)
+    {
+        return dp[stepsLeft][pos];
+    }
+    long long left = 0;
+    long long right = 0;
+
+    long long stay=solve(stepsLeft-1,pos,arrLen,dp);
+    if(pos+1<arrLen) right=solve(stepsLeft-1,pos+1,arrLen,dp); 
+    if(pos-1>=0) left=solve(stepsLeft-1,pos-1,arrLen,dp);
+
+    return dp[stepsLeft][pos]=(((stay%MOD)+(right%MOD))%MOD+(left%MOD))%MOD;
+}
+public:
+    int numWays(int steps, int arrLen) {
+
+        long long ans=0;
+        arrLen = min(arrLen, steps + 1);
+        
+
+        vector<vector<long long>>dp(steps+1,vector<long long>(arrLen,-1));
+        return solve(steps,0,arrLen,dp);
+
+        // solve(stepsLeft, pos);
+        // Number of valid ways to end at index 0 after stepsLeft remaining moves, when I'm currently at pos.
+        
+    }
+};
