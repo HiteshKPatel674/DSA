cd1c2845bec3848010095d989416e72ee02366fc
HiteshKPatel674 <147538687+HiteshKPatel674@users.noreply.github.com>
2026-07-15 11:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (46.67%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -9,19 +9,22 @@ public:
     int gcdOfOddEvenSums(int n) {
 
         int sum_even=0;
-        int itr1=0,itr2=0;
-        for(int i=2;itr1<n ;i=i+2)
-        {
-            sum_even+=i;
-            itr1++;
-        }
+        // int itr1=0,itr2=0;
+        // for(int i=2;itr1<n ;i=i+2)
+        // {
+        //     sum_even+=i;
+        //     itr1++;
+        // }
 
         int sum_odd=0;
-        for(int i=1;itr2<n ;i=i+2)
-        {
-            sum_odd+=i;
-            itr2++;
-        }
+        // for(int i=1;itr2<n ;i=i+2)
+        // {
+        //     sum_odd+=i;
+        //     itr2++;
+        // }
+
+        sum_even=(1+n)*n;
+        sum_odd=n*n;
 
         int ans=gcd(sum_even,sum_odd);
         return ans;
