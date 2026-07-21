f3a27d9a25619ac9e8bacbeffe49c70cd27033de
HiteshKPatel674 <147538687+HiteshKPatel674@users.noreply.github.com>
2026-07-21 14:32:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (5.46%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-     transform(s.begin(), s.end(), s.begin(), ::tolower); 
-     int j=0;
-     for(int i=0;i<s.size();i++)
-     {    if(isalnum(s[i])){
-         s[j++]=s[i];}
-     }
-        int start=0;
-        int end=j-1;//lenght of updated tsring without space  and ll lowercase
 
-        while(start<=end)
+        int n=s.size();
+        string str;
+        for(int i=0;i<n;i++)
         {
-            if(s[start++]!=s[end--]) return false;
+            if(isalnum(s[i])) str.push_back(tolower(s[i]));
         }
-        return true;
+        
+        string ori=str;
+        reverse(str.begin(),str.end());
+
+        return ori==str;
     }
 };
