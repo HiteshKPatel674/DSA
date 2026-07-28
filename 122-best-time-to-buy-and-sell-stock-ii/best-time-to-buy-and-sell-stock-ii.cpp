8159a34fa5e67fd0f2d8532f8f9598bc175665d0
HiteshKPatel674 <147538687+HiteshKPatel674@users.noreply.github.com>
2026-07-28 21:56:25 +0530

    Time: 15 ms (14.94%) | Memory: 27.5 MB (14.80%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    int solve(int index,vector<int>& prices,vector<vector<int>>&dp,int buy)
+    {
+        if(index==prices.size()) return 0;
+
+        if(dp[index][buy]!=-1) return dp[index][buy];
+
+        int profit=0;
+        if(buy)
+        {
+            int buykaro=-prices[index]+solve(index+1,prices,dp,0);
+            int skipkaro=0+solve(index+1,prices,dp,1);
+            profit=max(buykaro,skipkaro);
+        }
+        else
+        {
+            int sellkaro= +prices[index]+solve(index+1,prices,dp,1);
+            int skipkaro=0+solve(index+1,prices,dp,0);
+            profit=max(sellkaro,skipkaro);
+        }
+
+        return dp[index][buy]=profit;
+    }
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solve(0,prices,dp,1);
+        
+    }
+};
